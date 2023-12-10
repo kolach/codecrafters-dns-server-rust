1e21934cf33a17b9953e45b4ece872bc2fb19450
kolach <chikolad@gmail.com>
2023-12-10 15:51:12 -0500

    codecleanup

--- src/encoder.rs
+++ src/encoder.rs
@@ -205,56 +205,6 @@ impl<'a> Decoder<'a> {
         Ok(res)
     }
 
-    pub fn read_name(&mut self) -> Result<String, Error> {
-        let mut segments = Vec::new();
-
-        loop {
-            let len = self.read_u8()?;
-            if len == 0 {
-                break;
-            }
-
-            if len & 0xC0 == 0xC0 {
-                let offset = u16::from_be_bytes([len & 0x3F, self.read_u8()?]) as usize;
-                let original_ffset = self.set_offset(offset);
-                let segment = self.read_name()?;
-                segments.push(segment);
-                self.set_offset(original_ffset);
-            } else {
-                let bytes = self.read_slice(len as usize)?;
-                let label = std::str::from_utf8(bytes)?;
-                segments.push(label.into());
-            }
-        }
-
-        Ok(segments.join("."))
-    }
-
-    pub fn read_label(&mut self) -> Result<Option<&'a str>, Error> {
-        let label_offset = self.offset;
-
-        let len = self.read_u8()?;
-        if len == 0 {
-            return Ok(None);
-        }
-
-        if len & 0xC0 == 0xC0 {
-            let offset = u16::from_be_bytes([len & 0x3F, self.read_u8()?]) as usize;
-            let label = self.labels.get(&offset);
-            println!(
-                "compressed label detected! Offset: {}, found: {:?}",
-                offset, label
-            );
-            Ok(label.copied())
-        } else {
-            let bytes = self.read_slice(len as usize)?;
-            let label = std::str::from_utf8(bytes)?;
-            println!("writing label {} with offset {}", label, label_offset);
-            self.labels.insert(label_offset, label);
-            Ok(Some(label))
-        }
-    }
-
     pub fn read_u8(&mut self) -> Result<u8, Error> {
         let b = self.read_slice(1)?;
         Ok(b[0])
